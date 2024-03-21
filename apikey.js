67f9c84f1622ffe530f9e9c3ddff40309170c385
edicodes <125201811+edicodes@users.noreply.github.com>
2024-03-21 15:39:38 -0700

    apikey2

---
+++ apikey.js
@@ -0,0 +1,3 @@
+const OPENAI_API = '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';
+
+export default OPENAI_API;
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import OPENAI_API from "/apikey.js";
+
 const catalog = [
   {
     name: "Travel Jewelry Case",
